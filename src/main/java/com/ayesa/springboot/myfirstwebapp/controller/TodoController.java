f978a3fa32f7ad15d9481a713161e29177482f9f
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-11-01 11:07:59 +0100

    45. Step 15 - Getting first version of List Todos Page

---
+++ src/main/java/com/ayesa/springboot/myfirstwebapp/controller/TodoController.java
@@ -0,0 +1,35 @@
+package com.ayesa.springboot.myfirstwebapp.controller;
+
+import com.ayesa.springboot.myfirstwebapp.model.Todo;
+import com.ayesa.springboot.myfirstwebapp.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ *
+ * @author architecture - rperezv
+ * @version 01/11/2024 - 10:53
+ * @since jdk 1.17
+ */
+@Controller
+@RequiredArgsConstructor
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @GetMapping("/list-todos")
+    public String listAllTodos(ModelMap model) {
+
+        List<Todo> todos = todoService.retrieveTodos("in28minutes");
+
+        model.put("todos", todos);
+
+        return "listTodos";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>List Todos Page</title>
+    </head>
+    <body>
+        <h1>Welcome to sadasiva!</h1>
+        <div>Your todos are: ${todos}</div>
+    </body>
+</html>
