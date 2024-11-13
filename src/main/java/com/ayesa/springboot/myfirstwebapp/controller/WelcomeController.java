e4dcd2d8c91c897d558e229cb8aa44964c35899f
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-11-13 14:58:48 +0100

    64. Step 29 - Preparing for Spring Security

---
+++ src/main/java/com/ayesa/springboot/myfirstwebapp/controller/WelcomeController.java
@@ -0,0 +1,32 @@
+package com.ayesa.springboot.myfirstwebapp.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ *
+ * @author architecture - rperezv
+ * @version 27/10/2024 - 10:10
+ * @since jdk 1.17
+ */
+@Controller
+@RequiredArgsConstructor
+@SessionAttributes("name")
+@Slf4j
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String goToWelcomePage(ModelMap model) {
+        log.info("GoToLoginPage() (in controller) called!");
+
+        model.put("name", "in28minutes");
+
+        return "welcome";
+    }
+
+}
