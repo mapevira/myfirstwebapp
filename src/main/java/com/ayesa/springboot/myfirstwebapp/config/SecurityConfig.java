82a8f7580f8ebc67b65ef5fd4c5eb36c9b68aeea
Raul Perez Vicente <rperezv@ayesa.com>
2024-11-13 20:55:36 +0100

    72. Step 35 - 01 - Configuring Spring Security to Get H2 console Working

--- src/main/java/com/ayesa/springboot/myfirstwebapp/config/SecurityConfig.java
+++ src/main/java/com/ayesa/springboot/myfirstwebapp/config/SecurityConfig.java
@@ -2,11 +2,17 @@ package com.ayesa.springboot.myfirstwebapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -41,4 +47,19 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+
+        return http.build();
+    }
+
 }
